9ba35ad72cab6c63bb24df256ca0323a7fc49653
제호리 <je23ss@naver.com>
2025-08-22 01:55:38 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 기존 코드: st.title("My First App")
+# 수정 코드:
+st.title("🏫 우리반 알림장")
+st.write("우리 반의 중요한 소식을 확인하세요!")
